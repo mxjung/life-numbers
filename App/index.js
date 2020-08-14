81716eda83edddc1d08015264c1695a27a8c3dff
Max Jung <mjung@Peters-MacBook-Pro.local>
2020-08-14 00:15:16 -0700

    Changing folder structure, and adding welcome statement for application

---
+++ App/index.js
@@ -0,0 +1,29 @@
+import { StatusBar } from '../node_modules/expo-status-bar';
+import React from '../node_modules/react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>
+        Open up App.js to start working on your app! Got the Android Emulater working!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  welcome: {
+    fontSize: 20,
+    margin: 10,
+    textAlign: 'center',
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+});
